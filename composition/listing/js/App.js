70116edac84d5242004ba6541fd1fc5df833144f
Olga <olga.myazina@outlook.com>
2019-06-16 20:09:09 +0300

    hw-5

--- composition/listing/js/App.js
+++ composition/listing/js/App.js
@@ -1,16 +1,29 @@
 'use strict';
 
+const List = (props) => (
+  <div>
+    {props.children(props.items)}
+  </div>
+);
+
+const ListItem = ({...props}) => {
+  const item = props.children;
+  switch (item.type) {
+    case "unisex":
+      return <Item color="black" item={item}/>;
+    case "male":
+      return <Item color="blue" item={item}/>;
+    case "female":
+      return <Item color="orange" item={item}/>;
+  }
+};
+
 const App = ({items}) => (
   <main>
-    {items.map(item => {
-      switch(item.type) {
-        case 'unisex':
-          return <Item color="black" item={item} />;
-        case 'male':
-          return <Item color="blue" item={item} />;
-        case 'female':
-          return <Item color="orange" item={item} />;
+    <List items={items}>
+      {items =>
+        items.map((item, index) => <ListItem key={index}>{item}</ListItem>)
       }
-    })}
+    </List>
   </main>
 );
